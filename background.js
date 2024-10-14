f5d71e182b9df0607635be97f73bd400ecb4ef9a
Michał Matyas <github@higher.lv>
2024-10-14 13:36:02 +0200

    2.1.0: Corporate ad-driven garbage bullshit requirement update (converted the extension from manifest v2 to v3)

--- background.js
+++ background.js
@@ -1,190 +1,225 @@
-// @see http://stackoverflow.com/a/13818704/753815
-var STFUTabMatcher = function(ruleSet) {
-  this.ruleSet = ruleSet;
-}
+// background.js
 
-STFUTabMatcher.prototype = {
-  WILDCARD_MODE: 0,
-  REGEX_MODE:    1,
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
 
-  match: function(url) {
-    for(var i=0; i < this.ruleSet.length; i++) {
-      var rule = this.ruleSet[i];
-      if(!rule.regex) {
-        rule.regex = this.wildcardToRegex(rule.url);
-      }
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
 
-      if(url.match(rule.regex)) { 
-        return rule.mute;
-      }
-    }
-    return null;
-  },
-
-  wildcardToRegex: function(str) {
-    var quoted = (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + '-]', 'g'), '\\$&');
-    return new RegExp(quoted.replace(/\\\*/g, '.*').replace(/\\\?/g, '.'), 'g');
+self.addEventListener('message', function(event) {
+  if (event.data && event.data.type === 'INIT') {
+    initialize();
   }
-}
+});
 
-var Stfu = function() {
-  var self = this;
+function initialize() {
+  var STFUTabMatcher = function(ruleSet) {
+    this.ruleSet = ruleSet;
+  }
 
-  chrome.tabs.onCreated.addListener(function (tab) { 
-    self.initialAction(tab); 
-  });
-  chrome.tabs.onUpdated.addListener(function (tabId, changedInfo, tab) { 
-    if(changedInfo.url) {
-      self.initialAction(tab);
-    }
-    self.setIcon(tab); 
-  });
-  chrome.browserAction.onClicked.addListener(function(tab) { self.toggleMute(tab); });
+  STFUTabMatcher.prototype = {
+    WILDCARD_MODE: 0,
+    REGEX_MODE: 1,
 
-  chrome.storage.sync.get({muteTabsByDefault: true}, function (config) {
-    if (config.muteTabsByDefault === true) {
-      self.eachTab(function(tab) { self.setMute(tab, true); });
-    }
-  });
+    match: function(url) {
+      for (var i = 0; i < this.ruleSet.length; i++) {
+        var rule = this.ruleSet[i];
+        if (!rule.regex) {
+          rule.regex = this.wildcardToRegex(rule.url);
+        }
 
-  chrome.contextMenus.removeAll();
-  chrome.contextMenus.create({
-    title: "Mute all tabs",
-    contexts: ["browser_action"],
-    onclick: function() {
-      self.eachTab(function(tab) { 
-        self.setMute(tab, true) 
-      });
-    }
-  });
-
-  chrome.contextMenus.create({
-    title: "Unmute all tabs",
-    contexts: ["browser_action"],
-    onclick: function() {
-      self.eachTab(function(tab) { self.setMute(tab, false) });
-    }
-  });
-
-  chrome.contextMenus.create({
-    title: "Mute all other tabs",
-    contexts: ["browser_action"],
-    onclick: function() {
-      self.currentTab(function(current_tab) {
-        self.eachTab(function(tab) { self.setMute(tab, tab.id != current_tab.id) });
-      });
-    }
-  });
-
-  chrome.contextMenus.create({
-    title: "Unmute all other tabs",
-    contexts: ["browser_action"],
-    onclick: function() {
-      self.currentTab(function(current_tab) {
-        self.eachTab(function(tab) { self.setMute(tab, tab.id == current_tab.id) });
-      });
-    }
-  });
-
-  chrome.contextMenus.create({
-    title: "Blacklist this domain",
-    contexts: ["browser_action"],
-    onclick: function() {
-      self.currentTab(function(current_tab) {
-        chrome.storage.sync.get('ruleSet', function (config) {
-          var ruleSet = config.ruleSet;
-          var url = current_tab.url.split('/')
-          ruleSet.unshift({mute: true, url: url[0] + "//" + url[2] + "*"})
-          chrome.storage.sync.set({ruleSet: ruleSet });
-        });
-      });
-    }
-  });
-
-  chrome.contextMenus.create({
-    title: "Whitelist this domain",
-    contexts: ["browser_action"],
-    onclick: function() {
-      self.currentTab(function(current_tab) {
-        chrome.storage.sync.get('ruleSet', function (config) {
-          var ruleSet = config.ruleSet;
-          var url = current_tab.url.split('/')
-          ruleSet.unshift({mute: false, url: url[0] + "//" + url[2] + "*"})
-          chrome.storage.sync.set({ruleSet: ruleSet });
-        });
-      });
-    }
-  });
-
-  self.eachTab(function(tab) { self.setIcon(tab); });
-}
-
-Stfu.prototype = {
-  icons: { true: 'audio-muted.png', false: 'audio-on.png' },
-
-  setIcon: function(tab, muted) {
-    if(typeof muted == 'undefined') {
-      muted = tab.mutedInfo.muted
-    }
-    chrome.browserAction.setIcon({path: this.icons[muted], tabId: tab.id});
-  },
-
-  setMute: function(tab, muted) {
-    if(typeof muted == 'undefined') {
-      muted = true;
-    }
-
-    this.setIcon(tab, muted);
-    chrome.tabs.update(tab.id, {muted: muted});    
-  },
-
-  toggleMute: function(tab) {
-    this.setMute(tab, !tab.mutedInfo.muted);
-  },
-
-  eachTab: function (fn) {
-    chrome.tabs.query({}, function (tabs) {
-      for (var i = 0; i < tabs.length; i++) {
-        fn(tabs[i]);
+        if (url.match(rule.regex)) {
+          return rule.mute;
+        }
       }
-    });
-  },
+      return null;
+    },
 
-  currentTab: function (fn) {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-      fn(tabs[0]);
-    });
-  },
+    wildcardToRegex: function(str) {
+      var quoted = (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + '-]', 'g'), '\\$&');
+      return new RegExp(quoted.replace(/\\\*/g, '.*').replace(/\\\?/g, '.'), 'g');
+    }
+  }
 
-  initialAction: function(tab) {
+  var Stfu = function() {
     var self = this;
 
-    chrome.storage.sync.get(null, function (config) {
-      if(tab.url) {
-        var matcher = new STFUTabMatcher(config.ruleSet);
-        var match = matcher.match(tab.url);
+    chrome.tabs.onCreated.addListener(function(tab) {
+      self.initialAction(tab);
+    });
+    chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab) {
+      if (changedInfo.url) {
+        self.initialAction(tab);
+      }
+      self.setIcon(tab);
+    });
+    chrome.action.onClicked.addListener(function(tab) {
+      self.toggleMute(tab);
+    });
 
-        if(match !== null) {
-          self.setMute(tab, match);
-        } else {
-          if (tab.openerTabId) {
-            chrome.tabs.get(tab.openerTabId, function (opener_tab) {
-              var muted = config.muteTabsByDefault && opener_tab.mutedInfo.muted
-              self.setMute(tab, muted)            
-            });
-          } else if(config.muteTabsByDefault) {
-            self.setMute(tab, true)
-          } else {
-            self.setIcon(tab);
-          }
-        }
-      } else if(config.muteTabsByDefault) {
-        self.setMute(tab, true)
-      } else {
-        self.setIcon(tab);
+    chrome.storage.sync.get({ muteTabsByDefault: true }, function(config) {
+      if (config.muteTabsByDefault === true) {
+        self.eachTab(function(tab) {
+          self.setMute(tab, true);
+        });
       }
     });
+
+    chrome.contextMenus.removeAll();
+    chrome.contextMenus.create({
+      id: "muteAllTabs",
+      title: "Mute all tabs",
+      contexts: ["action"]
+    });
+
+    chrome.contextMenus.create({
+      id: "unmuteAllTabs",
+      title: "Unmute all tabs",
+      contexts: ["action"]
+    });
+
+    chrome.contextMenus.create({
+      id: "muteOtherTabs",
+      title: "Mute all other tabs",
+      contexts: ["action"]
+    });
+
+    chrome.contextMenus.create({
+      id: "unmuteOtherTabs",
+      title: "Unmute all other tabs",
+      contexts: ["action"]
+    });
+
+    chrome.contextMenus.create({
+      id: "blacklistDomain",
+      title: "Blacklist this domain",
+      contexts: ["action"]
+    });
+
+    chrome.contextMenus.create({
+      id: "whitelistDomain",
+      title: "Whitelist this domain",
+      contexts: ["action"]
+    });
+
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      if (info.menuItemId === "muteAllTabs") {
+        self.eachTab(function(tab) {
+          self.setMute(tab, true);
+        });
+      } else if (info.menuItemId === "unmuteAllTabs") {
+        self.eachTab(function(tab) {
+          self.setMute(tab, false);
+        });
+      } else if (info.menuItemId === "muteOtherTabs") {
+        self.currentTab(function(current_tab) {
+          self.eachTab(function(tab) {
+            self.setMute(tab, tab.id != current_tab.id);
+          });
+        });
+      } else if (info.menuItemId === "unmuteOtherTabs") {
+        self.currentTab(function(current_tab) {
+          self.eachTab(function(tab) {
+            self.setMute(tab, tab.id == current_tab.id);
+          });
+        });
+      } else if (info.menuItemId === "blacklistDomain") {
+        self.currentTab(function(current_tab) {
+          chrome.storage.sync.get('ruleSet', function(config) {
+            var ruleSet = config.ruleSet;
+            var url = current_tab.url.split('/');
+            ruleSet.unshift({ mute: true, url: url[0] + "//" + url[2] + "*" });
+            chrome.storage.sync.set({ ruleSet: ruleSet });
+          });
+        });
+      } else if (info.menuItemId === "whitelistDomain") {
+        self.currentTab(function(current_tab) {
+          chrome.storage.sync.get('ruleSet', function(config) {
+            var ruleSet = config.ruleSet;
+            var url = current_tab.url.split('/');
+            ruleSet.unshift({ mute: false, url: url[0] + "//" + url[2] + "*" });
+            chrome.storage.sync.set({ ruleSet: ruleSet });
+          });
+        });
+      }
+    });
+
+    self.eachTab(function(tab) {
+      self.setIcon(tab);
+    });
   }
+
+  Stfu.prototype = {
+    icons: { true: 'audio-muted.png', false: 'audio-on.png' },
+
+    setIcon: function(tab, muted) {
+      if (typeof muted == 'undefined') {
+        muted = tab.mutedInfo.muted;
+      }
+      chrome.action.setIcon({ path: this.icons[muted], tabId: tab.id });
+    },
+
+    setMute: function(tab, muted) {
+      if (typeof muted == 'undefined') {
+        muted = true;
+      }
+
+      this.setIcon(tab, muted);
+      chrome.tabs.update(tab.id, { muted: muted });
+    },
+
+    toggleMute: function(tab) {
+      this.setMute(tab, !tab.mutedInfo.muted);
+    },
+
+    eachTab: function(fn) {
+      chrome.tabs.query({}, function(tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+          fn(tabs[i]);
+        }
+      });
+    },
+
+    currentTab: function(fn) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        fn(tabs[0]);
+      });
+    },
+
+    initialAction: function(tab) {
+      var self = this;
+
+      chrome.storage.sync.get(null, function(config) {
+        if (tab.url) {
+          var matcher = new STFUTabMatcher(config.ruleSet);
+          var match = matcher.match(tab.url);
+
+          if (match !== null) {
+            self.setMute(tab, match);
+          } else {
+            if (tab.openerTabId) {
+              chrome.tabs.get(tab.openerTabId, function(opener_tab) {
+                var muted = config.muteTabsByDefault && opener_tab.mutedInfo.muted;
+                self.setMute(tab, muted);
+              });
+            } else if (config.muteTabsByDefault) {
+              self.setMute(tab, true);
+            } else {
+              self.setIcon(tab);
+            }
+          }
+        } else if (config.muteTabsByDefault) {
+          self.setMute(tab, true);
+        } else {
+          self.setIcon(tab);
+        }
+      });
+    }
+  }
+
+  STFU = new Stfu();
 }
 
-STFU = new Stfu;
+initialize();
--- options.js
+++ options.js
@@ -1,38 +1,37 @@
-chrome.runtime.getBackgroundPage(function (backgroundWindow) {
-  STFU = backgroundWindow.STFU;
+// options.js
 
-  chrome.storage.sync.get(null, function (config) {
-    $('#mute-tabs-by-default').prop('checked', config.muteTabsByDefault);
+chrome.storage.sync.get(null, function (config) {
+  $('#mute-tabs-by-default').prop('checked', config.muteTabsByDefault);
 
-    ruleSet = config.ruleSet || [];
+  ruleSet = config.ruleSet || [];
 
-    var blacklist = ruleSet.filter(function(rule) { return rule.mute; });
-    var whitelist = ruleSet.filter(function(rule) { return !rule.mute; });
+  var blacklist = ruleSet.filter(function(rule) { return rule.mute; });
+  var whitelist = ruleSet.filter(function(rule) { return !rule.mute; });
 
-    $('#blacklist').val(blacklist.map(function(rule) { return rule.url }).join('\n'));
-    $('#whitelist').val(whitelist.map(function(rule) { return rule.url }).join('\n'));
+  $('#blacklist').val(blacklist.map(function(rule) { return rule.url }).join('\n'));
+  $('#whitelist').val(whitelist.map(function(rule) { return rule.url }).join('\n'));
 
+  $('#save-changes').click(function(e) {
+    e.preventDefault();
+    var ruleSet = [], list = "";
 
-    $('#save-changes').click(function(e) {
-      e.preventDefault();
-      var ruleSet = [], list = "";
-
-      list = $('#whitelist').val();
-      list.trim().split('\n').forEach(function(line) {
-        var url = line.trim();
-        if(url.length > 0) { ruleSet.push({ mute: false, url: url }); }
-      });
-
-      list = $('#blacklist').val();
-      list.trim().split('\n').forEach(function(line) { 
-        var url = line.trim();
-        if(url.length > 0) { ruleSet.push({ mute: true, url: url }); }
-      });
-
-      chrome.storage.sync.set({
-        ruleSet: ruleSet, 
-        muteTabsByDefault: $('#mute-tabs-by-default').prop('checked')
-      });
+    list = $('#whitelist').val();
+    list.trim().split('\n').forEach(function(line) {
+      var url = line.trim();
+      if(url.length > 0) { ruleSet.push({ mute: false, url: url }); }
     });
+
+    list = $('#blacklist').val();
+    list.trim().split('\n').forEach(function(line) {
+      var url = line.trim();
+      if(url.length > 0) { ruleSet.push({ mute: true, url: url }); }
+    });
+
+    chrome.storage.sync.set({
+      ruleSet: ruleSet,
+      muteTabsByDefault: $('#mute-tabs-by-default').prop('checked')
+    });
+
+    chrome.runtime.sendMessage({ type: 'UPDATE_RULES', ruleSet: ruleSet, muteTabsByDefault: $('#mute-tabs-by-default').prop('checked') });
   });
 });
